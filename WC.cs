8769a6ff398a04436331391b9b176114dbfa5350
general <sgladkoskoka@gmail.com>
2023-12-22 17:31:44 +0200

    upload

---
+++ WC.cs
@@ -0,0 +1,11 @@
+﻿namespace Hospital
+{
+    public static class WC
+    {
+        public const string ImagePath = @"\images\doctor\";
+
+        public const string AdminRole = "Admin";
+        public const string UserRole = "User";
+        public const string DoctorRole = "Doctor";
+    }
+}
